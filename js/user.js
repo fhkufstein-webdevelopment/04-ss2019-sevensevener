e76a2265a3e375b0ad72c1d9fc70afceb8a6ee25
ChrisS49 <1810653109@stud.fh-kufstein.ac.at>
2019-04-08 18:51:26 +0200

    done

--- js/user.js
+++ js/user.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var userListBody = $('.userList tbody');
 
     //@todo store and somehow update the current number of users
-
+    let index = 0;
 
     $('.needs-validation').submit(function(event) {
 
@@ -27,20 +27,56 @@ $(document).ready(function() {
 
         //your code follows here
 
+            const username = document.querySelector('#username');
+            const newRow = document.createElement('tr');
+
+            if(username.value !== " "){
+                newRow.innerHTML = `
+                    <tr>
+                        <td>${++index}</td>
+                        <td>${username.value}</td>
+                        <td><button type="button" class="btn btn-secondary btn-danger deleteTrigger" title="Löschen"><i class="fa fa-trash"></i></button></td>
+                    </tr>
+                    `;
+
+                //append Child
+                userListBody.append(newRow);
+                //clear row
+                username.value = ' '; 
+            } else{
+                //...
+            }
+
+
         return false;
     });
 
 
-    $('.deleteTrigger').click(function() {
+    $(userListBody).on('click', '.deleteTrigger', function(e) {
         //@todo
         //1. remove current user from dom
         //2. update number of current users
 
         //your code follows here
-
-
+            if(confirm("Wirklich löschen?")){
+                console.log(e);
+                deleteRow(e.target);
+                updateIndex();
+            }
     });
 
     //maybe some code follows here
 
+    function deleteRow(target){
+            console.log('clicked');
+            target.parentElement.parentElement.remove(); 
+        }
+    
+    function updateIndex(){
+        //when i found out we should do it with jQuery :-)
+        userListBody.find('tr').each(function(ind) {
+            $(this).find('td:first-child').text(++ind);
+            index = ind;    //so new created rows will always start at latest id+1
+        });
+    }
 });
